7c0a69f04c56977db6af314f57b345885ddee193
Willem van der Graaf <w.g.vandergraaf@gmail.com>
2018-01-16 15:54:01 +0100

    Remove comments in test_module

--- test_module.py
+++ test_module.py
@@ -6,7 +6,3 @@ y = 3.
 # Test multiply function
 test = multiply(x, y)
 print(test)
-
-# Check hypothesis: If this change is not yet commited, does it move to the working area when I change branches?
-
-# Answer: Yes! I am now in the branch new-feature!
